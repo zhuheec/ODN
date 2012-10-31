abb142fd0fbea7e0a80b5d3799a875500bb5dcb9
Jay(He Zhu) <zhuheec@gmail.com>
2012-10-31 18:00:16 -0400

    Change project name to ODN_Analysis

--- ODN_Analysis/src/org/zh/odn/trace/A.java
+++ ODN_Analysis/src/org/zh/odn/trace/A.java
@@ -7,8 +7,13 @@ public class A {
 	}
 	
 	public static void main(String[] args) {
-		new A();
-		OdnTracer.printTrace();
+		//new A();
+		//OdnTracer.printTrace();
+		String s = "111::222";
+		String[] arr = s.split(":::");
+		for (String ss : arr) {
+			System.out.println(ss);
+		}
 	}
 }
 
